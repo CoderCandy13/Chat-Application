e644cbc2f670164c0839118dc7183d884d8763f3
sayalikailasmouli <Kailas_mouli@yahoo.com>
2023-04-07 21:23:59 +0530

    first commit

---
+++ src/chatsPage.js
@@ -0,0 +1,16 @@
+import { PrettyChatWindow } from "react-chat-engine-pretty";
+
+const ChatsPage = (props) => {
+  return (
+    <div style={{ height: "100vh", width: "100vw" }}>
+      <PrettyChatWindow
+        projectId="2132e29f-352e-48f1-9dea-cd7ded344010"
+        username={props.user.username} // adam
+        secret={props.user.secret} // pass1234
+        style={{ height: "100%" }}
+      />
+    </div>
+  );
+};
+
+export default ChatsPage;
